7124b38d636917976d13ced292951b886c75f37e
Joseph Nusairat <joseph@nusairat.com>
2020-10-10 23:23:17 -0700

    #comment asnyc iteration throguh files

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# File Find and Move
+
+I by accidentally wiped one of my external drives that had quite a bit of my pictures on it last year.
+
+So I recovered it but it produced a ton of files and I wasn't sure what to take.
+
+I realized all the files I needed were from my Sony and Canon Camera.
+
+So this app iterates through directories, checks certain files types first, then analyzes the EXIF to see if the make is Sony or Canon, then moves the file.
+
+In theory I should have also put a date range on it but didn't.
+
+I also hard coded hte paths, since I didn't care.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::collections::HashSet;
+use async_std::{task, fs};
+use async_std::path::{Path, PathBuf};
 use std::io;
 use std::ffi::OsStr;
 use std::io::BufReader;
@@ -15,75 +14,62 @@ const TO_PATH_MAIN: &str = "/Users/joseph/Downloads/Processed/Extra";
 fn main() -> io::Result<()> {
     println!("Anaylze Types!");
 
-    let (set, i) = search(FILE_PATH).unwrap();
-    println!("Files analyzed : {}", i);
+    process(FILE_PATH).unwrap();
 
-    // Now iterate and copy
-    for path in set {        
-        let val = format!("{}", get_extension_from_filename(&path).unwrap_or_else(|| "N/A"));
-        copy(&path, TO_PATH_MAIN, &val).unwrap();        
-    }
+    println!("DONE");
 
     Ok(())
 }
 
-fn search(path: &str) -> io::Result<(HashSet<PathBuf>, u32)> {
+fn process(path: &str) -> io::Result<()> {
     use regex::{Regex};
     use lazy_static::lazy_static;
-
-    let mut set = HashSet::new();
-    let mut i = 0;
+    use async_std::prelude::*;
 
 
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(?i)canon|sony").unwrap();
     }
 
-    for entry in fs::read_dir(path)? {
-        i += 1;
-        let entry = entry?;
-        let path = entry.path();
+    task::block_on(async {
+        let mut dir = fs::read_dir(&path).await?;
 
-        let metadata = fs::metadata(&path)?;
-        if metadata.file_type().is_file() {
-            let val = format!("{}", get_extension_from_filename(&path).unwrap_or_else(|| "N/A"));
-            if allowed_type(&val) {
-                // Now check to see if the image is from a Canon or Sony
-                // Lots of sloppy unwraps, but eh .. fails it fails
-                let file = File::open(path.to_str().unwrap()).unwrap();
-                let exifResult = Reader::new().read_from_container(
-                    &mut BufReader::new(&file));
-                match exifResult {
-                    Ok(exif) => {
-                        if let Some(field) = get_string(&exif, Tag::Make) {                    
-                            if RE.is_match(&field.to_lowercase()) {
-                                set.insert(path);
-                            }
-                        }                
-                    },
-                    Err(e) => {
-                        // PNG and a few i dont care about cant be processed by this file
-                        // mostly it was JPG and CR2
+        while let Some(res) = dir.next().await {
+            let entry = res?;
+            let path = entry.path();
+
+            let metadata = fs::metadata(&path).await?;
+            if metadata.file_type().is_file() {
+                let val = format!("{}", get_extension_from_filename(&path).unwrap_or_else(|| "N/A"));
+                if allowed_type(&val) {
+                    // Now check to see if the image is from a Canon or Sony
+                    // Lots of sloppy unwraps, but eh .. fails it fails
+                    let file = File::open(path.to_str().unwrap()).unwrap();
+                    let exif_result = Reader::new().read_from_container(
+                        &mut BufReader::new(&file));
+                    match exif_result {
+                        Ok(exif) => {
+                            if let Some(field) = get_string(&exif, Tag::Make) {                    
+                                if RE.is_match(&field.to_lowercase()) {
+                                    //set.insert(path);
+                                    println!("FOUND :: {:?}", path);
+                                    copy(&path, TO_PATH_MAIN, &val).await.unwrap();
+                                }
+                            }                
+                        },
+                        Err(_) => {
+                            // PNG and a few i dont care about cant be processed by this file
+                            // mostly it was JPG and CR2
+                        }
                     }
                 }
+            } 
+            else if metadata.file_type().is_dir() {
+                process(&path.to_str().unwrap()).unwrap();
             }
-        } 
-        else if metadata.file_type().is_dir() {
-            match search(&path.to_str().unwrap()) {
-                Ok(ret) => {
-                    let subset = ret.0;
-                    let x = ret.1;
-                    set.extend(subset);
-                    i += x;
-                },
-                Err(err) => {
-                    println!("Error : {:?} / {}", path, err);
-                }
-            }
-        }
-    }
-
-    return Ok((set, i));
+        }        
+        Ok(())
+    })    
 }
 
 fn get_string(reader: &Exif, tag: Tag) -> Option<String> {
@@ -97,22 +83,23 @@ fn get_extension_from_filename(path: &PathBuf) -> Option<&str> {
         .and_then(OsStr::to_str)
 }
 
-fn copy(full: &PathBuf, output_path: &str, extension: &str) -> io::Result<()> {
+async fn copy(full: &PathBuf, output_path: &str, extension: &str) -> io::Result<()> {
     let name = full.file_stem().unwrap().to_str().unwrap();
         
     let mut full_path = format!("{}/{}.{}", output_path, name, extension);
     let mut path = Path::new(full_path.as_str());
-    if path.exists() {
+
+    if path.exists().await {
         // add a random number to the front
         full_path = format!("{}/{}_{}.{}", output_path, random(), name, extension);
         path = Path::new(full_path.as_str());
     }
     
-    std::fs::copy(full, path).unwrap();
+    std::fs::copy(full, path)?;
     Ok(())
 }
 
-fn random() -> String {         // <5>
+fn random() -> String {        
     use rand::{thread_rng, Rng};
     use rand::distributions::Alphanumeric;
 
@@ -135,19 +122,19 @@ fn allowed_type(s : &str) -> bool {
     }
 }
 
-fn moved_type(s : &str) -> bool {
-    match s.to_lowercase().as_str() {
-        "mov" => true,
-        "png" => true,
-        "gif" => true,
-        "m4p" => true,
-        "jpg" => true,
-        "jpeg"=> true,
-        "cr2" => true,
-        "tif" => true,
-        "heic" => true,
-        "mp3" => true,
-        "mp4" => true,
-        _ => false
-    }
-}
+// fn moved_type(s : &str) -> bool {
+//     match s.to_lowercase().as_str() {
+//         "mov" => true,
+//         "png" => true,
+//         "gif" => true,
+//         "m4p" => true,
+//         "jpg" => true,
+//         "jpeg"=> true,
+//         "cr2" => true,
+//         "tif" => true,
+//         "heic" => true,
+//         "mp3" => true,
+//         "mp4" => true,
+//         _ => false
+//     }
+// }
